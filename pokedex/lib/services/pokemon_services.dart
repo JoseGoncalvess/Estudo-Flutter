f6cc17ff0bb48dac4705479d25997a26f2a2a130
José Gonçalves <joseinacio774@gmail.com>
2023-03-20 17:47:01 -0300

    Ajust no modelo

--- pokedex/lib/services/pokemon_services.dart
+++ pokedex/lib/services/pokemon_services.dart
@@ -9,11 +9,12 @@ class PokemonServices extends PokedexInterface {
     final dio = Dio();
     // String PokeImg =
     //     "https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/versions/generation-iv/platinum/1.png";
-    String pokeUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=493';
+    String pokeUrl =
+        'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
     var response = await dio.get(pokeUrl);
 
     if (response.statusCode == 200) {
-      Map<String, dynamic> map = response.data;
+      list = response.data;
 
       // list = List<PokeModel>.from(map.map((e) => PokeModel.fromJson(e)));
     }
